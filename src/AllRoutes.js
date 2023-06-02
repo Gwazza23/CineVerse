93d61d1f27883deb85cd9c95d006bfa92a57051e
Gwazza23 <weerawarnagayan@gmail.com>
2023-06-02 11:20:39 +0100

    create navbar

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
+import HomePage from "./Components/HomePage/HomePage";
 
 function AllRoutes() {
     const location = useLocation();
@@ -9,6 +10,7 @@ function AllRoutes() {
     <AnimatePresence mode="wait" initial={false}>
       <Routes location={location} key={location.pathname} >
         <Route path="/" element={<NavBar />} >
+          <Route index element={<HomePage />} />
         </Route>
       </Routes>
     </AnimatePresence>
