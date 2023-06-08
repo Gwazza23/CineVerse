aec64b76ac61e606fc58ecc19cd1cee0e6895d69
Gwazza23 <weerawarnagayan@gmail.com>
2023-06-08 11:20:43 +0100

    create MoviePage component

--- src/Pages/MoviePage/MoviePage.js
+++ src/Pages/MoviePage/MoviePage.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
 import BannerCard from "./Cards/BannerCard";
 
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getMovieDetails, selectMovies } from "../../Slices/MoviesSlice";
+
 function MoviePage() {
+  const dispatch = useDispatch();
   const {id} = useParams();
 
+  const movie = useSelector(selectMovies).data
+
+  useEffect(() => {
+    dispatch(getMovieDetails(id))
+  }, [dispatch, id])
+
   return <div>
-    <BannerCard />
+    <BannerCard movie={movie} />
   </div>;
 }
 
